18998637b780428fbfa8448c899aad9e6c9d4a76
somasekhardevalla <98879851+somasekhardevalla@users.noreply.github.com>
2023-02-14 23:11:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Forcaste from './components/Forcaste';
+
+function App() {
+  return (
+    <div className="App">
+   <Forcaste/>
+    </div>
+  );
+}
+
+export default App;
